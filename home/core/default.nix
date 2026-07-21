cdae61e3f6454969591c4b2d724cbadb6814319b
TristanGrlt <tristan.groult@gmail.com>
2026-07-22 00:32:31 +0200

    refactor default termianls tools

--- home/core/default.nix
+++ home/core/default.nix
@@ -2,38 +2,5 @@
 
 {
   home.packages = with pkgs; [
-    # Default commande line tools
-    
-    nnn		# terminal file manager
-
-    ###
-    # archives
-    ###
-    zip
-    unzip
-
-    ###
-    # utils
-    ###
-    gnugrep	# find in files
-    ripgrep	# faster grep
-    jq		# JSON parser
-
-    ###
-    # networking
-    ###
-    mtr		# ping + traceroute
-    dnsutils	# dig + nslookup
-    nmap	# network discovery
-    ipcalc	# calculator for IPs
-
-    ###
-    # misc
-    ###
-    file	# file informations
-    which	# binary localisation
-    tree	# print directory tree
-    glow	# CLI md renderer
-    
   ];
 }
--- modules/core/default.nix
+++ modules/core/default.nix
@@ -24,6 +24,40 @@
     vim
     git
     wget
+    tmux
+
+    # Default commande line tools
+    
+    nnn		# terminal file manager
+
+    ###
+    # archives
+    ###
+    zip
+    unzip
+
+    ###
+    # utils
+    ###
+    gnugrep	# find in files
+    ripgrep	# faster grep
+    jq		# JSON parser
+
+    ###
+    # networking
+    ###
+    mtr		# ping + traceroute
+    dnsutils	# dig + nslookup
+    nmap	# network discovery
+    ipcalc	# calculator for IPs
+
+    ###
+    # misc
+    ###
+    file	# file informations
+    which	# binary localisation
+    tree	# print directory tree
+    glow	# CLI md renderer
   ];
   environment.variables.EDITOR = "vim";
 }

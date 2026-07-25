f3475fc13de01476a6bcb752f1d1a9a95f84d6fd
TristanGrlt <tristan.groult@gmail.com>
2026-07-25 18:05:18 +0200

    reduce font in terminal

--- users/tristan/home.nix
+++ users/tristan/home.nix
@@ -28,7 +28,7 @@
     settings = {
       env.TERM = "xterm-256color";
       font = {
-        size = 12;
+        size = 11;
         draw_bold_text_with_bright_colors = true;
       };
       scrolling.multiplier = 5;

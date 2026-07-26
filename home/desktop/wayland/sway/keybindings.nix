93c55949ecbd9b46dcbae5092641e1a557a4487c
TristanGrlt <tristan.groult@gmail.com>
2026-07-26 18:30:54 +0200

    fix(sway/keybindings): add media and volume control keybinds

--- home/desktop/wayland/sway/keybindings.nix
+++ home/desktop/wayland/sway/keybindings.nix
@@ -8,5 +8,16 @@
     "Mod4+Shift+q" = "kill";				# kill selected window
     "Mod4+Shift+c" = "reload";				# reload sway
     "Mod4+Mod1+l" = "exec swaylock -f -c 000000";	# lock the session
+
+    # Media control
+    "XF86AudioPlay" = "exec ${pkgs.playerctl}/bin/playerctl play-pause";
+    "XF86AudioPause" = "exec ${pkgs.playerctl}/bin/playerctl play-pause";
+    "XF86AudioNext" = "exec ${pkgs.playerctl}/bin/playerctl next";
+    "XF86AudioPrev" = "exec ${pkgs.playerctl}/bin/playerctl previous";
+
+    # Volume control
+    "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
+    "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
+    "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
   };
 }

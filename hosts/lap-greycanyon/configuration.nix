ed7cf8b299ce0b80c0807f715da5693516b6bd7c
TristanGrlt <tristan.groult@gmail.com>
2026-07-26 18:44:00 +0200

    fix(lap-greycanyon/user): remove unwanted comma

--- hosts/lap-greycanyon/configuration.nix
+++ hosts/lap-greycanyon/configuration.nix
@@ -17,7 +17,7 @@
   users.users."${username}" = {
     isNormalUser = true;
     description = "${username}";
-    extraGroups = [ "networkmanager" "wheel", "video" ];
+    extraGroups = [ "networkmanager" "wheel" "video" ];
   };
 
   system.stateVersion = "26.05";

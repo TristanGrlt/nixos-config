10466738c54295d88945ab2829e62f1663301616
TristanGrlt <tristan.groult@gmail.com>
2026-07-26 17:53:59 +0200

    feat(kanshi): configure kanshi to my screens

---
+++ users/tristan/kanshi.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+
+{
+  services.kanshi = {
+    enable = true;
+    
+    settings = [
+      {
+        profile = {
+          name = "nomade";
+          outputs = [
+            { 
+              criteria = "eDP-1"; # Ton laptop garde généralement ce nom
+              status = "enable"; 
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "home";
+          outputs = [
+            { 
+              criteria = "eDP-1"; 
+              position = "0,0"; 
+              scale = 1.10;
+            }
+            { 
+              criteria = "Dell Inc. DELL U2424H HNJJL04"; 
+              position = "375,0";
+              status = "enable";
+              scale = 0.90;
+              mode = "1920x1080@120Hz";
+            }
+          ];
+        };
+      }
+    ];
+  };
+}

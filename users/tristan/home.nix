f3bcc6b9621616649584dc1b8839e96f6816544a
TristanGrlt <tristan.groult@gmail.com>
2026-07-26 14:04:53 +0200

    delete unused term var

--- users/tristan/home.nix
+++ users/tristan/home.nix
@@ -30,7 +30,6 @@
       env.TERM = "xterm-256color";
       font = {
         size = 11;
-        draw_bold_text_with_bright_colors = true;
       };
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;

dcc8690e7a2a7cdd5bf7349a70ff1cd9ecd5589c
TristanGrlt <tristan.groult@gmail.com>
2026-07-26 19:08:05 +0200

    fix(fonts): fix google sourced fonts

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -1,5 +1,21 @@
 { pkgs, ... }:
 
+let
+  space-grotesk = pkgs.stdenvNoCC.mkDerivation {
+    name = "space-grotesk";
+    dontConfigure = true;
+    src = pkgs.fetchzip {
+      url = "https://github.com/floriankarsten/space-grotesk/releases/download/2.0.0/SpaceGrotesk-2.0.0.zip";
+      stripRoot = false;
+      # Force une erreur pour exposer le hash SRI
+      hash = "sha256-niwd5E3rJdGmoyIFdNcK5M9A9P2rCbpsyZCl7CDv7I8=";
+    };
+    installPhase = ''
+      mkdir -p $out/share/fonts/opentype
+      find $src -name "*.otf" -exec cp {} $out/share/fonts/opentype/ \;
+    '';
+  };
+in
 {
   fonts = {
     packages = with pkgs; [

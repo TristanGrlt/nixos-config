03a37b2dac46aae47e69878aa9975a77aed67390
TristanGrlt <tristan.groult@gmail.com>
2026-07-30 00:29:29 +0200

    feat(lockscreen): add conf for hyprlock

--- home/desktop/wayland/lock.nix
+++ home/desktop/wayland/lock.nix
@@ -1,27 +1,85 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+  c = config.theme.colors;
+  font_family = "SpaceMono Nerd Font";
+
+  # Convert "#RRGGBB" to "rgb(RRGGBB)" for Hyprlock parser
+  hex = color: "rgb(" + builtins.substring 1 6 color + ")";
+in
 {
-  programs.hyprlock.enable = true;
-
-  # Auto activation config
-  services.swayidle = {
+  programs.hyprlock = {
     enable = true;
-    
-    events = [
-      { event = "before-sleep"; command = "${pkgs.hyprlock}/bin/hyprlock"; }
-      { event = "lock"; command = "${pkgs.hyprlock}/bin/hyprlock"; }
-    ];
-    
-    timeouts = [
-      { 
-        timeout = 300; 
-        command = "${pkgs.hyprlock}/bin/hyprlock"; 
-      }
-      { 
-        timeout = 600; 
-        command = "${pkgs.sway}/bin/swaymsg 'output * dpms off'"; 
-        resumeCommand = "${pkgs.sway}/bin/swaymsg 'output * dpms on'"; 
-      }
-    ];
+
+    settings = {
+      general = {
+        disable_loading_bar = true;
+        hide_cursor = false;
+      };
+
+      background = [
+        {
+          monitor = "";
+          path = "${config.theme.wallpaper}";
+          color = hex c.bg;
+          blur_passes = 2;
+          blur_size = 4;
+        }
+      ];
+
+      input-field = [
+        {
+          monitor = "";
+          size = "300, 50";
+
+          # Force square borders and password dots
+          rounding = 1;
+          dots_rounding = 0;
+
+          dots_size = 0.30;
+          dots_spacing = 0.22;
+          dots_center = true;
+
+          inner_color = hex "#ffffff";
+          font_color = hex c.bg;
+
+          fade_on_empty = false;
+          placeholder_text = "Password...";
+          hide_input = false;
+
+          check_color = hex c.yellow;
+
+          fail_color = hex c.red;
+          fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
+
+          position = "0, -100";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+
+      label = [
+        {
+          monitor = "";
+          text = "$TIME";
+          color = hex "#ffffff";
+          font_size = 90;
+          font_family = "${font_family} Bold";
+          position = "0, 100";
+          halign = "center";
+          valign = "center";
+        }
+        {
+          monitor = "";
+          text = ''cmd[update:18000000] date +"<b>%A %-d %B</b>"'';
+          color = hex "#ffffff";
+          font_size = 20;
+          font_family = "${font_family}";
+          position = "0, 10";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+    };
   };
 }

bcbe8d7806e4239ac74c54eefe5f6d5921e3788d
TristanGrlt <tristan.groult@gmail.com>
2026-09-20 15:25:42 +0200

    fix(wifi): make config persistent trought reboot

--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -1,4 +1,4 @@
 { username, ... }: {
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
-  sops.age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
+  sops.age.keyFile = "/var/lib/sops-nix/keys.txt";
 }
